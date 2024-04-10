caa01a811e354277461f082de74cc969d057cc97
camilagsoares <camila.soares19@hotmail.com>
2024-04-09 21:43:36 -0300

    changes components

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -31,7 +31,7 @@ export class ProductDetailsComponent implements OnInit {
     this.productService.getProductById(productId)
       .subscribe(product => {
         this.productDetails = product;
-        this.editedTitle = product.title; // Initialize editedTitle with current title
+        this.editedTitle = product.title;
       });
   }
 
@@ -47,24 +47,19 @@ export class ProductDetailsComponent implements OnInit {
   saveChanges() {
     if (this.productId) {
       const updatedData = {
-        title: this.editedTitle,
-        // Adicione outras propriedades atualizadas conforme necessário
+        title: this.editedTitle
       };
-  
+
       this.productService.updateProduct(this.productId, updatedData)
         .subscribe(
           (updatedProduct) => {
-            // A edição foi bem-sucedida, a resposta da API deve conter o produto atualizado
-            console.log('Produto atualizado com sucesso:', updatedProduct);
-            this.productDetails = updatedProduct;
+            this.productDetails.title = updatedProduct.title;
             this.isEditing = false;
-            // Forçar a detecção de mudanças
+            this.isEditingTitle = false;
             this.cdr.detectChanges();
           },
           (error) => {
-            // Houve um erro ao tentar atualizar o produto
             console.error('Erro ao atualizar produto:', error);
-            // Você pode exibir uma mensagem de erro para o usuário aqui
           }
         );
     }
